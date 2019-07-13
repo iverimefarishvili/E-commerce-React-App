86f760c1758ec8d259023af6da5dc37b14055cd4
iveri <mefarishvili922@gmail.com>
2019-07-14 00:05:16 +0400

    e-commerce

--- src/components/product.js
+++ src/components/product.js
@@ -1,6 +1,23 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
+import storeProducts from './data';
 class Product extends React.Component{
+   render(){
+      return(
+         
 
+         <div className="col-9 mx-auto com-md-6 col-lg-3 my-3">
+            <div className="card">
+               <div className="img-container p-">
+                  <Link to="/details">
+                     <img src={this.props.name.img} />
+                  </Link>
+                  <button className="cart-btn" disabled={this.props.name.inCaart ? true : false }/>
+               </div>
+               
+            </div>   
+         </div>
+      )
+   } 
 }
 export default Product;
--- src/components/productslist.js
+++ src/components/productslist.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Product from './product';
 import Title from './title';
+import storeProducts from './data';
+
+
+
 class Productslist extends React.Component{
+   state = {
+      products: storeProducts
+   }
    render(){
-       return(
-          <Title name="sda" title="asdas"></Title>
-       )
+      
+      return(
+         <div>
+            {
+               this.state.products.map(items => {
+                  return <Product name={items} />
+               })
+            }
+         </div>
+      )
    } 
 }
 export default Productslist;
